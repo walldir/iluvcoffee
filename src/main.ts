ade643f2912a7320588d8f2ebc419bd6dc54e394
Jose Waldir <jose.waldir@applydigital.co>
2023-05-27 15:33:05 -0300

    Add validation pipes, refactor controller and service to use dtos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // this will remove any properties that are not in the DTO
+      forbidNonWhitelisted: true, // this will throw an error if there are properties that are not in the DTO
+      transform: true, // this will transform the incoming data to the DTO type
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
